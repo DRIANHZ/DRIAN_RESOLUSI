9afa6f8628d37db80e4980c54b79190cdd2ae26b
DRIANHZ <146804114+DRIANHZ@users.noreply.github.com>
2023-10-06 05:40:41 +0700

    menambhkan nabi adam

--- perjuangan.php
+++ perjuangan.php
@@ -1,5 +1,6 @@
 <?php 
 $koneksi = mysqli_connect("localhost","root","","allah");
+$nabi = mysqli_connect("localhost","root","","adam");
 $query = mysqli_query($koneksi,"SELECT * FROM tb_dunia_akhirat");
 connect rosullah
 ?>
