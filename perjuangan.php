d9c9ad880a9e69ef6c2ee5400ca1ef69a7babe2b
DRIANHZ <146804114+DRIANHZ@users.noreply.github.com>
2023-10-06 05:39:32 +0700

    hehe

--- perjuangan.php
+++ perjuangan.php
@@ -1,6 +1,6 @@
 <?php 
 $koneksi = mysqli_connect("localhost","root","","allah");
-$koneksi = mysqli_connect("localhost","root","","ibrahim");
+$hidayah = mysqli_connect("localhost","root","","ibrahim");
 $query = mysqli_query($koneksi,"SELECT * FROM tb_dunia_akhirat");
 
 connect rosullah
